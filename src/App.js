cbe74de455f1a3610ab45b78c73b7dce93c85bc9
JanhaviMakwana <janhavimakwana305@gmail.com>
2021-04-30 18:29:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Map from './components/GoogleMap/GoogleMap';
+
 import './App.css';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `600px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { withGoogleMap, withScriptjs, GoogleMap, InfoWindow } from 'react-google-maps';
+
+const API_KEY = "8724f6361faef2c68171d5561b48b8b2";
+
+const Map = withScriptjs(withGoogleMap((props) => {
+
+    const [position, setPosition] = useState({ lat: 28.58, lng: 77.204 });
+    const [weather, setWeather] = useState({ place: '', country: '', humidity: '', tempMax: '', tempMin: '' });
+    const [show, setShow] = useState(false);
+
+    const onClickMap = (e) => {
+
+        setShow(true);
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+        setPosition({
+            lat: lat,
+            lng: lng
+        });
+        showWeatherInfo();
+    }
+
+    const showWeatherInfo = () => {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.lat}&lon=${position.lng}&appid=${API_KEY}`)
+            .then(res => {
+                console.log(res);
+                setWeather({
+                    place: res.data.name,
+                    country: res.data.sys.country,
+                    humidity: res.data.main.humidity,
+                    tempMax: res.data.main.temp_max,
+                    tempMin: res.data.main.temp_min,
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <div>
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={position}
+                onClick={onClickMap}
+
+            >
+                {show && <InfoWindow
+
+                    position={{ lat: position.lat, lng: position.lng }}
+                >
+                    <div>
+                        <p>Place: {weather.place}</p>
+                        <p>Country: {weather.country}</p>
+                        <p>Humidity: {weather.humidity}</p>
+                        <p>Max Temperature: {weather.tempMax}</p>
+                        <p>Min Temperature: {weather.tempMin}</p>
+                    </div>
+                </InfoWindow>}
+            </GoogleMap>
+        </div>
+    );
+}));
+
+export default Map;
